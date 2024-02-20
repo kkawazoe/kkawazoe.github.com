0c824800ff6c40afe7c2db99a2808a584221cf30
kkawazoe <kkawazoe@users.noreply.github.com>
2024-02-20 08:24:23 +0000

    deploy: 077db1ebd08fc352ce901dfa840a8ebde17aa596

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 
 var data = [
   {
+    url: "https://kkawazoe.github.io/blog/2024/02/20/how-to-get-a-list-of-registered-users-in-amazon-iam/",
+    title: "Amazon IAM で登録されている関数一覧を取得する方法",
+    image: "images/logo/aws_logo.png",
+    date: "2024-02-20",
+    body: "Amazon IAM で登録されている関数一覧を取得する方法 Amazon IAM で登録されているユーザ一覧を取得する方法について調査した内容を備忘録として残しておく 方法 該当の AWS Console にログインして AWS Cloud Shell 上で以下のコマンドを実行する 実際のコード aws iam list-users \\ | jq -r \u0026#39;.Users[] | [.UserName, .CreateDate, .PasswordLastUsed] | @csv\u0026#39; \\ | tr -d \u0026#39;\u0026#34;\u0026#39; \\ \u0026gt; iam_users_list.csv"
+  },
+  {
     url: "https://kkawazoe.github.io/blog/2024/02/08/configuring-rancher-desktop/",
     title: "Rancher Desktop の設定について",
     image: "images/logo/docker_logo.png",
@@ -42,13 +49,6 @@ var data = [
     image: "images/logo/aws_logo.png",
     date: "2024-01-29",
     body: "Amazon Cognito でコマンドからユーザ登録およびパスワード設定を行う方法 Amazon Cognito でコマンドからユーザ登録およびパスワード設定を行う方法について調査した内容を備忘録として残しておく 方法 該当の AWS Console にログインして AWS Cloud Shell 上で以下のコマンドを実行する 実際のコード ユーザ登録 \u0026ndash;user-attributes は必要に応じて増やす ※「\u0026ndash;message-action SUPPRESS」のオプションを付与することで対象ユーザへの通知を止めることが可能 aws cognito-idp admin-create-user \\ --user-pool-id {ユーザプール ID} \\ --username {E メールアドレス ※E メールアドレスをユーザ名に設定した場合} \\ --user-attributes Name=email,Value={E メールアドレス} \\ Name=email_verified,Value=TRUE \\ --message-action SUPPRESS パスワード設定 \u0026ndash;no-permanent は仮パスワード扱い ※「\u0026ndash;permanent」オプションを付与で恒久パスワード扱いになる aws cognito-idp admin-set-user-password \\ --user-pool-id {ユーザプール ID} \\ --username {ユーザ名 or sub} \\ --password {password} \\ --no-permanent"
-  },
-  {
-    url: "https://kkawazoe.github.io/blog/2024/01/20/how-to-get-a-list-of-registered-users-in-amazon-iam/",
-    title: "Amazon IAM で登録されている関数一覧を取得する方法",
-    image: "images/logo/aws_logo.png",
-    date: "2024-01-20",
-    body: "Amazon IAM で登録されている関数一覧を取得する方法 Amazon IAM で登録されているユーザ一覧を取得する方法について調査した内容を備忘録として残しておく 方法 該当の AWS Console にログインして AWS Cloud Shell 上で以下のコマンドを実行する 実際のコード aws iam list-users \\ | jq -r \u0026#39;.Users[] | [.UserName, .CreateDate, .PasswordLastUsed] | @csv\u0026#39; \\ | tr -d \u0026#39;\u0026#34;\u0026#39; \\ \u0026gt; iam_users_list.csv"
   },
   {
     url: "https://kkawazoe.github.io/blog/2024/01/17/tips-about-aws/",
