4f80bad0b2829743f7e079183cde124753bb87c9
kkawazoe <kkawazoe@users.noreply.github.com>
2024-02-20 08:17:53 +0000

    deploy: 29c32d2d179586d48c36f7de98662543cbd902f1

--- index.js
+++ index.js
@@ -44,6 +44,13 @@ var data = [
     body: "Amazon Cognito でコマンドからユーザ登録およびパスワード設定を行う方法 Amazon Cognito でコマンドからユーザ登録およびパスワード設定を行う方法について調査した内容を備忘録として残しておく 方法 該当の AWS Console にログインして AWS Cloud Shell 上で以下のコマンドを実行する 実際のコード ユーザ登録 \u0026ndash;user-attributes は必要に応じて増やす ※「\u0026ndash;message-action SUPPRESS」のオプションを付与することで対象ユーザへの通知を止めることが可能 aws cognito-idp admin-create-user \\ --user-pool-id {ユーザプール ID} \\ --username {E メールアドレス ※E メールアドレスをユーザ名に設定した場合} \\ --user-attributes Name=email,Value={E メールアドレス} \\ Name=email_verified,Value=TRUE \\ --message-action SUPPRESS パスワード設定 \u0026ndash;no-permanent は仮パスワード扱い ※「\u0026ndash;permanent」オプションを付与で恒久パスワード扱いになる aws cognito-idp admin-set-user-password \\ --user-pool-id {ユーザプール ID} \\ --username {ユーザ名 or sub} \\ --password {password} \\ --no-permanent"
   },
   {
+    url: "https://kkawazoe.github.io/blog/2024/01/20/how-to-get-a-list-of-registered-users-in-amazon-iam/",
+    title: "Amazon IAM で登録されている関数一覧を取得する方法",
+    image: "images/logo/aws_logo.png",
+    date: "2024-01-20",
+    body: "Amazon IAM で登録されている関数一覧を取得する方法 Amazon IAM で登録されているユーザ一覧を取得する方法について調査した内容を備忘録として残しておく 方法 該当の AWS Console にログインして AWS Cloud Shell 上で以下のコマンドを実行する 実際のコード aws iam list-users \\ | jq -r \u0026#39;.Users[] | [.UserName, .CreateDate, .PasswordLastUsed] | @csv\u0026#39; \\ | tr -d \u0026#39;\u0026#34;\u0026#39; \\ \u0026gt; iam_users_list.csv"
+  },
+  {
     url: "https://kkawazoe.github.io/blog/2024/01/17/tips-about-aws/",
     title: "AWS についての Tips",
     image: "images/logo/aws_logo.png",
@@ -58,11 +65,11 @@ var data = [
     body: "Amazon Cognito でユーザプールに登録されているユーザ一覧を取得する方法 Amazon Cognito でユーザプールに登録されているユーザ一覧を取得する方法について調査した内容を備忘録として残しておく 方法 該当の AWS Console にログインして AWS Cloud Shell 上で以下のコマンドを実行する 実際のコード aws cognito-idp list-users \\ --user-pool-id {該当のユーザプール ID} \\ --filter \u0026#34;{検索に使用する属性} = \\\u0026#34;{検索文字列}\\\u0026#34;\u0026#34; \\ --attributes-to-get \u0026#34;{取得する属性(スペース区切りで複数指定可)}\u0026#34; \\ | jq -r \u0026#39;.Users[] | [.Username, .Attributes[].Value] | @csv\u0026#39; \\ | tr -d \u0026#39;\u0026#34;\u0026#39; \\ \u0026gt; user_pool_list.csv"
   },
   {
-    url: "https://kkawazoe.github.io/blog/2024/01/16/how-to-get-a-list-of-registered-functions-in-amazon-cognito/",
-    title: "Amazon Lamdba で登録されている関数一覧を取得する方法",
+    url: "https://kkawazoe.github.io/blog/2024/01/16/how-to-get-a-list-of-registered-functions-in-amazon-lambda/",
+    title: "Amazon Lambda で登録されている関数一覧を取得する方法",
     image: "images/logo/aws_logo.png",
     date: "2024-01-16",
-    body: "Amazon Lamdba で登録されている関数一覧を取得する方法 Amazon Lamdba で登録されている関数一覧を取得する方法について調査した内容を備忘録として残しておく 方法 該当の AWS Console にログインして AWS Cloud Shell 上で以下のコマンドを実行する 実際のコード aws lambda list-functions --region {リージョン名} \\ | jq \u0026#39;.Functions[] | [.FunctionName]\u0026#39; \\ | jq -rs \u0026#39;map([.[]])[] | @csv\u0026#39; \\ | tr -d \u0026#39;\u0026#34;\u0026#39; \u0026gt; lambda-functions-list.csv"
+    body: "Amazon Lambda で登録されている関数一覧を取得する方法 Amazon Lambda で登録されている関数一覧を取得する方法について調査した内容を備忘録として残しておく 方法 該当の AWS Console にログインして AWS Cloud Shell 上で以下のコマンドを実行する 実際のコード aws lambda list-functions --region {リージョン名} \\ | jq \u0026#39;.Functions[] | [.FunctionName]\u0026#39; \\ | jq -rs \u0026#39;map([.[]])[] | @csv\u0026#39; \\ | tr -d \u0026#39;\u0026#34;\u0026#39; \\ \u0026gt; lambda-functions-list.csv"
   },
   {
     url: "https://kkawazoe.github.io/blog/2023/12/28/how-to-send-push-notifications-with-amazon-pinpoint-api/",
